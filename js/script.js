6810e110f378cb75e3654f17b983e8ff15103333
jclaw <jclaw3456@gmail.com>
2016-11-23 17:33:01 -0500

    data loading in d3

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
 $(document).ready(function() {
     var categories = ['age', 'sex of workers', 'class of worker', 'industry', 'occupation']
 
-    cleanStateData(StateData)
+    cleanStateData(StateData);
+    console.log(StateData);
 
     function tooltipHtml(n, d) {	/* function to create html content string in tooltip div. */
-        return "<h4>"+n+"</h4><table>"+
-            "<tr><td>Low</td><td>"+(d.low)+"</td></tr>"+
-            "<tr><td>Average</td><td>"+(d.avg)+"</td></tr>"+
-            "<tr><td>High</td><td>"+(d.high)+"</td></tr>"+
-            "</table>";
+        console.log(d);
+        var str = '<h4>' + n + '</h4><table>';
+        for (var i = 0; i < d.length; i++) {
+            str += '<tr><td>' + d[i].code + '</td><td>' + d[i].value + '</td/></tr>'
+        }
+        str += '</table';
+        return str;
     }
 
     $('#variables').html('');
@@ -18,8 +21,7 @@ $(document).ready(function() {
         optgroup.attr('label', category);
         partition[category].variables.forEach(function(item) {
             var option = $('<option>');
-            var text = item.normalized_label;
-            option.val(text).text(text);
+            option.val(item.code).text(item.normalized_label);
             optgroup.append(option);
         })
         $('#variables').append(optgroup);
@@ -28,14 +30,14 @@ $(document).ready(function() {
     $('#variables').change(function() {
         var selected = $(':selected', this);
         var category = selected.closest('optgroup').attr('label');
-        var variable = selected.val();
-        applyData(category, variable);
+        var code = selected.val();
+        applyData(category, code);
     })
 
 
     // applyData('','');
 
-    function applyData(category, variable) {
+    function applyData(category, code) {
         var sampleData ={};	/* Sample random data. */
         ['AL','AK','AZ','AR','CA','CO','CT','DE','DC','FL',
          'GA','HI','ID','IL','IN','IA','KS','KY','LA','ME',
@@ -49,14 +51,21 @@ $(document).ready(function() {
         //  "CO", "NM", "OR", "ND", "SD", "NE", "IA", "MS", "IN", "IL", "MN",
         //  "WI", "MO", "AR", "OK", "KS", "LS", "VA"]
             .forEach(function(state) {
-                console.log(state, ': ', category, ': ', variable);
-                sampleData[state] = StateData[state][category]
-                var datapoint = StateData[state][category][variable]
-                sampleData[state].color = d3.interpolate("#ffffcc", "#800026")(StateData[state][category][variable]/100);
+                console.log(state, ': ', category, ': ', code);
+                sampleData[state] = {};
+                sampleData[state].variables = []
+                for (key in StateData[state][category]) {
+                    sampleData[state].variables.push({'code': key, 'value': StateData[state][category][key]})
+                }
+
+                var datapoint = StateData[state][category][code];
+                var wtotal = StateData[state][category]['wtotal'];
+                console.log(datapoint, wtotal);
+                sampleData[state].color = d3.interpolate("#ffffcc", "#800026")(datapoint / wtotal);
             });
 
         /* draw states on id #statesvg */
-        uStates.draw("#statesvg", StateData, tooltipHtml);
+        uStates.draw("#statesvg", sampleData, tooltipHtml);
 
         d3.select(self.frameElement).style("height", "600px");
     }
@@ -66,9 +75,12 @@ $(document).ready(function() {
             for (var category in data[state]) {
                 delete data[state][category]['NAME']
                 for (key in data[state][category]) {
+                    data[state][category][key] = parseInt(data[state][category][key])
                     if (key.includes('_001E')) {
-                        var val = data[state][category][key]
-                        data[state][category]['total'] = val
+                        data[state][category]['total'] = data[state][category][key]
+                        delete data[state][category][key]
+                    } else if (c_to_v(key).normalized_label == 'Worked at home') {
+                        data[state][category]['wtotal'] = data[state][category][key]
                         delete data[state][category][key]
                     }
                 }
